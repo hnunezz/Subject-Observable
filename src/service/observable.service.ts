579c3eb0253ff33d31941b2cd37112f2809c6da7
hnunezz <nunesdealmeidahenrique@gmail.com>
2022-06-29 21:00:01 -0300

    Fix repo

---
+++ src/service/observable.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ObservableService {
+
+  public observable: Subject<any>;
+
+  constructor() {
+    this.observable = new Subject<any>();
+  }
+
+  public setObservable(value: any) {
+    this.observable.next(value);
+    //.next => indicates that changing the object will reflect on whoever is subscribed to the observable
+  }
+
+  public getObservable(): Observable<any> {
+    return this.observable.asObservable();
+    // return the observable value and where do you sign up to receive the changes
+  }
+}
